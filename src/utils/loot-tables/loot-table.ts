7fb9ec93c7eb662bcfa9a0c2cc81f6c978adf64d
DidaS <didasoficial@gmail.com>
2024-09-20 05:18:27 +0100

    Partially fix the loot table code
    This is still broken, check line 62 for details

--- src/utils/loot-tables/loot-table.ts
+++ src/utils/loot-tables/loot-table.ts
@@ -7,11 +7,32 @@ import type { LootTableContent } from "./types.js";
 export abstract class LootTable {
     public contents: Array<LootTableContent> = [];
 
+    #findClosest(arr: Array<LootTableContent>, target: number): LootTableContent {
+        let left = 0;
+        let right = arr.length - 1;
+
+        while (left < right) {
+            let pl = arr[left].probability;
+            let pr = arr[right].probability;
+
+            // If the probabilities are equal we want to randomly reduce one of them by a small value
+            if (pl === pr) {
+                const shouldReduceLeft = Math.round(Math.random()) === 1;
+                if (shouldReduceLeft) pl -= 0.0000000001;
+                else pr -= 0.0000000001;
+            }
+
+            if (Math.abs(pl - target) <= Math.abs(pr - target)) right--;
+            else left++;
+        }
+
+        return arr[left];
+    }
+
     public getResults(amount: number): Array<LootTableContent> {
         const uniqueDrops = new Set();
-        const rv: Array<LootTableContent> = [];
+        const result: Array<LootTableContent> = [];
         const droppable: Array<LootTableContent> = [];
-        const droppableMap: Array<number> = [];
 
         let alwaysCount = 0;
 
@@ -19,39 +40,51 @@ export abstract class LootTable {
             const object = this.contents[i];
             if (!object.enabled) continue;
             if (object.always) {
-                rv.push(object);
+                if (object.allowExtra) droppable.push(object);
+                result.push(object);
                 alwaysCount++;
                 continue;
             }
 
             droppable.push(object);
-            droppableMap.push(object.probability);
         }
 
+        // This not only makes it easier to get the highest probability
+        // but it also helps to fix some odd behavior that you would
+        // otherwise encounter with the find closest function
+        droppable.sort((a, b) => a.probability - b.probability);
+
         const realDropCount = amount - alwaysCount;
-        const probabilitySum = droppable.reduce((a, b) => a + b.probability, 0);
+        const biggestProbability = droppable.at(-1)?.probability ?? 1;
 
         if (realDropCount > 0) {
-            let runningValue = 0;
             for (let i = 0; i < realDropCount; i++) {
-                const hitValue = getRandomArbitrary(0, probabilitySum);
+                //! TODO Remake the way to get the hit value
+                // When being fully random we get a really standard distribution
+                // This pretty much means that probabilities are not properly respected
+                // when there are few items in the table
+                // ex: with an item of probability 50 and one with probability 1 you would expect to see 50 for each 1
+                // however with this random system we still get a 50/50 distribution (which is expected)
+                //
+                // The hit value should be somewhat aware of the way probabilities should be distributed
+                const hitValue = getRandomArbitrary(0, biggestProbability + 1);
+                const object = this.#findClosest(droppable, hitValue);
 
-                for (let j = 0, { length } = droppable; j < length; j++) {
-                    const object = droppable[j];
-                    runningValue += object.probability;
-
-                    if (hitValue < runningValue) {
-                        if (object.unique && uniqueDrops.has(object)) continue;
-                        if (object.unique) uniqueDrops.add(object);
-
-                        rv.push(object);
-
-                        break;
+                if (object.unique) {
+                    if (uniqueDrops.has(object)) {
+                        // Even tho the item is already in the drops
+                        // we still want to get the specified amount of items
+                        i--;
+                        continue;
                     }
+
+                    uniqueDrops.add(object);
                 }
+
+                result.push(object);
             }
         }
 
-        return <never>rv;
+        return <never>result;
     }
 }
